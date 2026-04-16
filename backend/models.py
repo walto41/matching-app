4e231a7a902b5398350fc4f6d2d5467aca9d7676
walto41 <89987630+walto41@users.noreply.github.com>
2026-04-16 02:18:52 +0000

    final update

---
+++ backend/models.py
@@ -0,0 +1,25 @@
+from extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    bio = db.Column(db.String(300))
+    profile_pic = db.Column(db.String(300), nullable=True)
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    liker_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    liked_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user1_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user2_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    timestamp = db.Column(db.DateTime, server_default=db.func.now())
