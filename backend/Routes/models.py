ec83c1a728d696fe2d107b26626551aab2bcf9d6
walto41 <89987630+walto41@users.noreply.github.com>
2026-04-17 18:11:59 +0000

    Final

--- backend/Routes/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    bio = db.Column(db.String(300))
